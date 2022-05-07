c276166b61ab5b1e6b1da6edfd270753e0267f75
Jessika <72019869+JessikaGyllstrom@users.noreply.github.com>
2022-05-07 23:29:18 +0200

    planet years

--- script.js
+++ script.js
@@ -57,3 +57,21 @@ function getColorBand (string) {
 console.log(getColorBand("Black")); //0
 console.log(getColorBand("brown")); //1
 console.log(getColorBand("violet")); //7 
+
+// Space Age
+// Given an age in seconds, calculate how old someone would be:
+function spaceAge (planet, seconds) {
+    const earthYear = 31557600;
+    const planets = {
+        Earth: 1,
+        Mercury: 0.2408467,
+        Venus: 0.61519726,
+        Mars: 1.8808158,
+        Jupiter: 11.862615, 
+        Saturn: 29.447498,
+        Uranus: 84.016846,
+        Neptune: 164.79132,
+    }
+    return Number((seconds/earthYear/planets[planet])).toFixed(2);
+}
+console.log(spaceAge("Earth", 1000000000));
