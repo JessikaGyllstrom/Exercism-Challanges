7fae252bf3610383158888c1ed0bff32981f433d
Jessika <72019869+JessikaGyllstrom@users.noreply.github.com>
2022-05-07 23:09:45 +0200

    Convert a resistor bands color to its numeric representation

--- script.js
+++ script.js
@@ -30,3 +30,30 @@ function regExTest (string) {
 }
 console.log(regExTest('The quick brown fox jumps over the lazy dog.')); //true
 console.log(regExTest('The quick qrown fox qumps over the qazy dog.')); //true
+
+// Convert a resistor bands color to its numeric representation 
+function getColorBand (string) {
+    if (/[a-z]/.test(string.charAt(0))) {
+        string = string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+    }
+    const resistor = {
+        Black: 0, 
+        Brown: 1, 
+        Red: 2, 
+        Orange: 3, 
+        Yellow: 4, 
+        Green: 5, 
+        Blue: 6, 
+        Violet: 7, 
+        Grey: 8, 
+        White: 9,
+    }
+    for (const [key, value] of Object.entries(resistor)) {
+        if (string == key) {
+           return `${value}`;
+        }
+    }
+}
+console.log(getColorBand("Black")); //0
+console.log(getColorBand("brown")); //1
+console.log(getColorBand("violet")); //7 
